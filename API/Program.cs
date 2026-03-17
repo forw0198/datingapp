4b36b633802fcf0666069a98827753e032d9f425
forw0198 <forw0198@gmail.com>
2026-03-17 15:51:05 +0200

    add angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+
 app.MapControllers();
 
 app.Run();
