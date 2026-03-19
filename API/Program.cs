f4e5d2465207aff1abb3ab391dbcd68caecc87bd
For Work <forw0198@email.com>
2026-03-19 15:04:41 +0200

    continue section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,8 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+        policy.AllowAnyHeader().AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200");
     });
 });
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,20 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 export class App implements OnInit {
   private http = inject(HttpClient);
   // protected readonly title = signal('client');
-  protected readonly title = 'DatinApp';
+  protected readonly title = 'Datin App';
+  protected members = signal<any>([]);
   
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request completed')
-    });
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
   }
+
+  async getMembers(){
+    try {
+      return firstValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
 }
